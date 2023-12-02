4dab1af13734f6d83cc48010d81f3561600727f2
ahmadAK111 <148124278+ahmadAK111@users.noreply.github.com>
2023-12-02 20:48:22 +0300

    Update User.py

--- User.py
+++ User.py
@@ -1,71 +1,127 @@
 import tkinter as tk
 from tkinter import messagebox
+import datetime
+from Signup import SignUp  # Import the SignUp class from the Signup file
 
-def reserve_cart():
-    # Retrieve selected college, start time, and end time from user inputs
-    college = college_var.get()
-    start_time = start_time_entry.get()
-    end_time = end_time_entry.get()
+class User:
+    def __init__(self, user_type):
+        self.user_type = user_type
+        self.reservations = []  # Store reservations for the user
 
-    # Retrieve user type from a global variable or any other appropriate source
-    user_type = get_user_type()
+    def calculate_duration(self, start_time, end_time):
+        duration = end_time - start_time
+        return duration
 
-    # Perform reservation duration validation
-    # Adjust the duration rules based on user type (student, employee, faculty)
-    max_duration = 30 if user_type == "Student" else 60 if user_type == "Employee" else 90
-    duration = calculate_duration(start_time, end_time)
-    if duration > max_duration:
-        messagebox.showerror("Reservation Error", "Reservation duration exceeds the allowed limit.")
-        return
+    def reserve_cart(self):
+        college = college_var.get()
+        start_time = start_time_entry.get()
+        end_time = end_time_entry.get()
 
-    # Check cart availability and perform the reservation
-    if check_cart_availability(college, start_time, end_time):
-        reserve_cart_in_database(college, start_time, end_time)
-        messagebox.showinfo("Reservation Success", "Cart reserved successfully.")
-    else:
-        messagebox.showerror("Reservation Error", "No available carts during the given time & date.")
-def show_reservations():
-    # Retrieve and display the list of active reservations for the current user
-    reservations = retrieve_user_reservations()
-    # Display the reservations in a suitable format (e.g., in a listbox or a table)
+        try:
+            start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+            end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            messagebox.showerror("Reservation Error", "Invalid date and time format. Please use the format YYYY-MM-DD HH:MM:SS.")
+            return
 
-def logout():
-    # Perform logout functionality, such as closing the current window and returning to the sign-up window
-    pass
+        max_duration = datetime.timedelta(minutes=30)
+        if self.get_user_type() == "Employee":
+            max_duration = datetime.timedelta(hours=1)
+        elif self.get_user_type() == "Faculty":
+            max_duration = datetime.timedelta(hours=1, minutes=30)
+
+        if end_time < start_time:
+            messagebox.showerror("Reservation Error", "End time cannot be earlier than start time.")
+            return
+
+        duration = self.calculate_duration(start_time, end_time)
+        if duration > max_duration:
+            max_hours = max_duration.total_seconds() // 3600
+            max_minutes = (max_duration.total_seconds() % 3600) // 60
+            messagebox.showerror("Reservation Error", f"Reservation duration exceeds the allowed limit of {int(max_hours)} hours and {int(max_minutes)} minutes.")
+            return
+
+        if self.check_cart_availability(college, start_time, end_time):
+            self.reserve_cart_in_database(college, start_time, end_time)
+            messagebox.showinfo("Reservation Success", "Cart reserved successfully.")
+        else:
+            messagebox.showerror("Reservation Error", "No available carts during the given time & date.")
+
+    def show_reservations(self):
+        if self.reservations:
+            reservation_messages = []
+            for index, reservation in enumerate(self.reservations, start=1):
+                start_time = reservation["start_time"].strftime("%Y-%m-%d %H:%M:%S")
+                end_time = reservation["end_time"].strftime("%Y-%m-%d %H:%M:%S")
+                reservation_message = f"Reservation {index}:\nStart Time: {start_time}\nEnd Time: {end_time}"
+                reservation_messages.append(reservation_message)
+            messagebox.showinfo("Reservations", "\n\n".join(reservation_messages))
+        else:
+            messagebox.showinfo("Reservations", "No reservations found.")
+
+    def logout(self):
+        # Close the current window
+        window.destroy()
+
+        # Create a new instance of the SignUp class to display the Signup window
+        signup_window = SignUp()
+
+    def get_user_type(self):
+        return self.user_type
+
+    def check_cart_availability(self, college, start_time, end_time):
+        # Replace this example logic with your own cart availability check
+        # Example implementation (replace with your own logic)
+        if college == "College of Science" and start_time.hour >= 8 and end_time.hour <= 16:
+            return True
+        elif college == "College of Computer Science and Information" and start_time.hour >= 9 and end_time.hour <= 17:
+            return True
+        elif college == "College of Engineering" and start_time.hour >= 10 and end_time.hour <= 18:
+            return True
+        else:
+            return False
+
+    def reserve_cart_in_database(self, college, start_time, end_time):
+        # Implement cart reservation in the database
+        reservation = {"start_time": start_time, "end_time": end_time}
+        self.reservations.append(reservation)
 
 # Create the main window
 window = tk.Tk()
 window.title("KSUGolfCarts")
 
 # Create the college selection dropdown
-colleges = ["College of Since ", " College of Computer Scince And Information ", " College of Engenerinig  "]  # Replace with actual college names
+colleges = ["College of Science", "College of Computer Science and Information", "College of Engineering"]  # Replace with actual college names
 college_var = tk.StringVar()
 college_dropdown = tk.OptionMenu(window, college_var, *colleges)
 college_dropdown.pack()
 
 # Create start time input field
-start_time_label = tk.Label(window, text="Enter start time and date of the reservation:")
+start_time_label = tk.Label(window, text="Enter start time and date of the reservation (YYYY-MM-DD HH:MM:SS):")
 start_time_label.pack()
-start_time_entry = tk.Entry(window)
+start_time_entry = tk.Entry(window, bd=5)
 start_time_entry.pack()
 
 # Create end time input field
-end_time_label = tk.Label(window, text="Enter end time and date of the reservation:")
+end_time_label = tk.Label(window, text="Enter end time and date of thereservation (YYYY-MM-DD HH:MM:SS):")
 end_time_label.pack()
-end_time_entry = tk.Entry(window)
+end_time_entry = tk.Entry(window, bd=5)
 end_time_entry.pack()
 
-# Create reserve button
-reserve_button = tk.Button(window, text="Reserve", command=reserve_cart)
-reserve_button.pack()
+# Create the user object
+user = User("Employee")  # Replace "Employee" with the actual user type
 
-# Create show button
-show_button = tk.Button(window, text="Show Reservations", command=show_reservations)
-show_button.pack()
+# Create the reserve cart button
+reserve_cart_button = tk.Button(window, text="Reserve Cart", command=user.reserve_cart)
+reserve_cart_button.pack()
 
-# Create logout button
-logout_button = tk.Button(window, text="Logout", command=logout)
+# Create the show reservations button
+show_reservations_button = tk.Button(window, text="Show Reservations", command=user.show_reservations)
+show_reservations_button.pack()
+
+# Create the logout button
+logout_button = tk.Button(window, text="Logout", command=user.logout)
 logout_button.pack()
 
-# Run the main event loop
+# Start the main event loop
 window.mainloop()
