a626e4ea248950980c08cbfe8c8a7002fcb02b8c
ahmadAK111 <148124278+ahmadAK111@users.noreply.github.com>
2023-12-05 23:47:59 +0300

    Update User.py

--- User.py
+++ User.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 import datetime
-  # Import the SignUp class from the Signup file
+import CentralDatabase as cd
+import sqlite3
 
 class User:
     def __init__(self, user_type):
@@ -14,28 +15,35 @@ class User:
 
     def reserve_cart(self):
         college = college_var.get()
+        start_time = start_time_entry.get()  # Retrieve the value from the entry field
+        end_time = end_time_entry.get()  # Retrieve the value from the entry field
 
-        cd.cursor.execute('''SELECT PlateNumber FROM RESERVATION WHERE PlateNumber = ?''', (college,))
-        result=cd.cursor.fetchone()
+        cd.cursor.execute('''SELECT College FROM RESERVATION WHERE College = ?''', (college,))
+        result =  cd.cursor.execute('''SELECT College FROM RESERVATION WHERE College = ?''', (college,))
 
         if result is not None:
-            # PlateNumber exists in the RESERVATION table
-            print("PlateNumber exists")
-            cd.cursor.execute("INSERT INTO RESERVATION (College) VALUES (?)", (college,))
+            # College exists in the RESERVATION table
+            print("College exists")
+            cd.cursor.execute("INSERT INTO RESERVATION (PlateNumber, College) SELECT PlateNumber, College FROM GOLF_CART;")
+            ab=(" SELECT College FROM RESERVATION WHERE College = (College)")
+            cd.cursor.execute("DELETE FROM GOLF_CART WHERE (College) = (?)", (college,))
             cd.conn.commit()
-            #cd.cursor.execute('''INSERT INTO RESERVATION (AccountID)VALUES(?)''', (a,))
+            window.destroy()
+            import User
+
         else:
-            # PlateNumber does not exist in the RESERVATION table
-            print("PlateNumber does not exist")
-
-
+            # College does not exist in the RESERVATION table
+            print("College does not exist")
+            print("College exists")
 
         cd.conn.commit()
         try:
             start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
             end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
         except ValueError:
-            messagebox.showerror("Reservation Error", "Invalid date and time format. Please use the format YYYY-MM-DD HH:MM:SS.")
+            messagebox.showerror(
+                "Reservation Error", "Invalid date and time format. Please use the format YYYY-MM-DD HH:MM:SS."
+            )
             return
 
         max_duration = datetime.timedelta(minutes=30)
@@ -52,7 +60,10 @@ class User:
         if duration > max_duration:
             max_hours = max_duration.total_seconds() // 3600
             max_minutes = (max_duration.total_seconds() % 3600) // 60
-            messagebox.showerror("Reservation Error", f"Reservation duration exceeds the allowed limit of {int(max_hours)} hours and {int(max_minutes)} minutes.")
+            messagebox.showerror(
+                "Reservation Error",
+                f"Reservation duration exceeds the allowed limit of {int(max_hours)} hours and {int(max_minutes)} minutes."
+            )
             return
 
         if self.check_cart_availability(college, start_time, end_time):
@@ -62,6 +73,10 @@ class User:
             messagebox.showerror("Reservation Error", "No available carts during the given time & date.")
 
     def show_reservations(self):
+        import CentralDatabase as cd
+
+                #cursor.execute("SELECT * , FROM RESERVATION")
+
         if self.reservations:
             reservation_messages = []
             for index, reservation in enumerate(self.reservations, start=1):
@@ -74,81 +89,73 @@ class User:
             messagebox.showinfo("Reservations", "No reservations found.")
 
     def logout(self):
-
         # Close the current window
         window.destroy()
 
         # Create a new instance of the SignUp class to display the Signup window
-        from signup import SignUp
+        from SignUp import SignUp
         signup_window = SignUp()
 
     def get_user_type(self):
         return self.user_type
 
-    def check_cart_availability(self):
+    def check_cart_availability(self, college, start_time, end_time):
         # Replace this example logic with your own cart availability check
         # Example implementation (replace with your own logic)
-        import login
-        import CentralDatabase as cd
+        pass
 
     def reserve_cart_in_database(self, college, start_time, end_time):
         # Implement cart reservation in the database
         reservation = {"start_time": start_time, "end_time": end_time}
         self.reservations.append(reservation)
 
+
 # Create the main window
 window = tk.Tk()
 window.title("KSUGolfCarts")
+window.configure(bg='#374aa1')
+
 
 # Create the college selection dropdown
-import CentralDatabase as cd
-
 cursor = cd.conn.cursor()
-
-# Execute the query
 cursor.execute('SELECT College FROM GOLF_CART')
-
-# Fetch all the rows
 rows = cursor.fetchall()
-depts = ('IS', 'CS', 'CE', 'IT', 'SE')
-
-# Create a list to store college names
-temp = [row[0] for row in rows]
-colleges = list(set(temp))
-
-# Create a Tkinter StringVar to store the selected college
+colleges = list((row[0] for row in rows))
 college_var = tk.StringVar()
-
-# Create the college dropdown menu
 college_dropdown = tk.OptionMenu(window, college_var, *colleges)
 college_dropdown.pack()
 
+
 # Create start time input field
-start_time_label = tk.Label(window, text="Enter start time and date of the reservation (YYYY-MM-DD HH:MM:SS):")
+start_time_label = tk.Label(window, text="Enter start time and date of the reservation (YYYY-MM-DD HH:MM:SS):",bg='#374aa1')
 start_time_label.pack()
 start_time_entry = tk.Entry(window, bd=5)
 start_time_entry.pack()
 
+
 # Create end time input field
-end_time_label = tk.Label(window, text="Enter end time and date of thereservation (YYYY-MM-DD HH:MM:SS):")
+end_time_label = tk.Label(window, text="Enter end time and date of the reservation (YYYY-MM-DD HH:MM:SS):" , bg='#374aa1')
 end_time_label.pack()
 end_time_entry = tk.Entry(window, bd=5)
 end_time_entry.pack()
 
-# Create the user object
-user = User("Employee")  # Replace "Employee" with the actual user type
+# Create the User instance
+user = User("Student")  # Replace "Employee" with the appropriate user type
 
-# Create the reserve cart button
-reserve_cart_button = tk.Button(window, text="Reserve Cart", command=user.reserve_cart)
-reserve_cart_button.pack()
+# Create the Reserve Cart button
+reserve_button = tk.Button(window, text="Reserve Cart", command=user.reserve_cart)
+reserve_button.pack()
 
-# Create the show reservations button
+# Create the Show Reservations button
 show_reservations_button = tk.Button(window, text="Show Reservations", command=user.show_reservations)
 show_reservations_button.pack()
 
-# Create the logout button
+# Create the Logout button
 logout_button = tk.Button(window, text="Logout", command=user.logout)
 logout_button.pack()
 
-# Start the main event loop
+# Run the main event loop
 window.mainloop()
+User=User()
+User.run()
+
